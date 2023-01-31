46ea193850bb2fcb783b7face43fd86d93f11b71
Austin Jones <austin.jones231@gmail.com>
2023-01-31 16:57:55 -0500

    Initial commit

---
+++ include/add.h
@@ -0,0 +1,12 @@
+#ifndef ADD_H
+#define ADD_H
+
+/**
+ * Calculates the sum of two integers (a+b).
+ * @param int a     One of the operands to be summed.
+ * @param int b     The other operand to be summed.
+ * @returns int     The sum of the two operands.
+ */
+int add(int a, int b);
+
+#endif
